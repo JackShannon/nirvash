149a862bd32ddee68f36ca9a040799b918e8f334
Colby Klein <shakesoda@gmail.com>
2012-08-23 20:20:43 -1000

    premake stuff for building GLFW. Cocoa and Win32 don't work correctly, and this should really generate the config header properly instead.

--- premake4.lua
+++ premake4.lua
@@ -12,6 +12,51 @@ project "TinyThread++"
 	files { "lib/tinythread/*.cpp" }
 	links { "pthread" }
 
+project "GLFW"
+	targetname "glfw3-tls"
+	targetdir "bin"
+	kind "StaticLib"
+	language "C"
+	files {
+		"lib/glfw/src/*.c"
+	}
+	-- these should all use X11, dunno if BSD/Solaris work, though.
+	if os.get() == "bsd"
+		or os.get() == "linux"
+		or os.get() == "solaris" then
+		links { "Xrandr", "Xf86vidmode", "X11", "GL" }
+		excludes {
+			"lib/glfw/src/cocoa_*",
+			"lib/glfw/src/win32_*"
+		}
+		os.copyfile(
+			"lib/glfw/src/config.h.x11", 
+			"lib/glfw/src/config.h"
+		)
+	elseif os.get() == "macosx" then
+		links { "OpenGL.framework" }
+		excludes {
+			"lib/glfw/src/x11_*",
+			"lib/glfw/src/win32_*"
+		}
+		os.copyfile(
+			"lib/glfw/src/config.h.cocoa", 
+			"lib/glfw/src/config.h"
+		)
+	elseif os.get() == "windows" then
+		links { "opengl32" }
+		excludes {
+			"lib/glfw/src/cocoa_*",
+			"lib/glfw/src/x11_*"
+		}
+		os.copyfile(
+			"lib/glfw/src/config.h.win32", 
+			"lib/glfw/src/config.h"
+		)
+	else
+		print "Invalid OS detected. Not sure what to think of this."
+	end
+
 project "Nepgear" 
 	targetname "nepgear"
 	targetdir "bin"
@@ -19,7 +64,10 @@ project "Nepgear"
 	language "C++"
 	files { "src/**.cpp" }
 	libdirs { "lib/glfw" }
-	links { "pthread", "glfw3", "X11", "GL", "Xrandr", "TinyThread++" }
+	links {
+		"pthread", "TinyThread++",
+		"GLFW", "Xrandr", "X11", "GL"
+	}
 	includedirs {
 		"lib/glfw/include",
 		"lib/glm",
