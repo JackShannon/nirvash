d5899f05277545af9b2d450354ee4a88cf5faf90
Colby Klein <shakesoda@gmail.com>
2012-08-24 21:38:40 -1000

    remove leftovers

--- src/main.cpp
+++ src/main.cpp
@@ -160,36 +160,7 @@ void loader_thread(void *data)
 
 	tdata->done = true;
 }
-/*
-bool Mesh::LoadMesh(const string& Filename)
-{
-    // Create the VAO
-    glGenVertexArrays(1, &m_VAO);
-    glBindVertexArray(m_VAO);
 
-    // Create the buffers for the vertices attributes
-    glGenBuffers(ARRAY_SIZE_IN_ELEMENTS(m_Buffers), m_Buffers);
-
-    bool Ret = false;
-
-    m_pScene = m_Importer.ReadFile(Filename.c_str(), aiProcess_Triangulate |
-		aiProcess_GenSmoothNormals | aiProcess_FlipUVs);
-
-    if (m_pScene) {
-       m_GlobalInverseTransform = m_pScene->mRootNode->mTransformation;
-       m_GlobalInverseTransform.Inverse();
-       Ret = InitFromScene(m_pScene, Filename);
-    }
-    else {
-       printf("Error parsing '%s': '%s'\n", Filename.c_str(), m_Importer.GetErrorString());
-    }
-
-    // Make sure the VAO is not changed from the outside
-    glBindVertexArray(0);
-
-    return Ret;
-} 
-*/
 void start_video(void *data)
 {
 	Nepgear::State *ng = (Nepgear::State*)data;
